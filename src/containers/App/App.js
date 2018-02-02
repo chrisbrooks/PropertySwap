7d1db1f56391f241dd0174d18d47d4dddf68654a
Chris Brooks <chrisbrooks1986@gmail.com>
2018-02-02 11:42:47 +1100

    making app stateless component

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,28 +1,19 @@
-/* eslint-disable */
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import ToastWrapper from 'components/Shared/Toast/ToastWrapper';
 import Header from 'components/Shared/Header/Header';
 import styles from './app.scss';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div>
-        <ToastWrapper />
-        <Header />
-        <div className={styles.Container}>
-          { this.props.children }
-        </div>
-      </div>
-    );
-  }
-}
+export const App = ({ children }) => (
+  <div>
+    <ToastWrapper />
+    <Header />
+    <div className={styles.Container}>
+      {children}
+    </div>
+  </div>
+);
 
 App.propTypes = {
   children: PropTypes.object
